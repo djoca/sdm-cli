33810a2b85e5f2984b5ea75b9c69248221fb6787
Jose Antonio Rosa dos Santos Junior <joses.jr@gmail.com>
2019-09-10 16:38:34 -0300

    sdm-group.sh script

---
+++ sdm-group.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+   echo "Usage: sdm-group.sh <ACCESS_KEY> <GROUP_NAME>"
+   exit 0
+fi
+
+ACCESS_KEY=$1; shift
+GROUP_NAME=$(echo $1 | sed "s/ /%20/g"); shift
+ARGS=$@
+
+COMMON_NAME=$(echo $GROUP_NAME | sed "s/ /%20/g")
+
+RESPONSE=$(curl -s \
+   -H "X-AccessKey: $ACCESS_KEY" \
+   "http://sjkap754:8050/caisd-rest/grp/COMMON_NAME-$COMMON_NAME")
+
+GROUP_ID=$(echo $RESPONSE | sed "s/[^']*'\([A-F0-9]\+\).*/\1/g")
+
+if [ "$ARGS" == "-x" ]; then
+    echo $RESPONSE
+else
+    echo $GROUP_ID
+fi
+
+if [ -z "$GROUP_ID" ]; then
+   echo "Group not found."
+   exit 1
+fi
+
