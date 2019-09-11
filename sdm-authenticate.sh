fadf0b7b19c89146e99c50652b9b8d0381289a5c
Jose Antonio Rosa dos Santos Junior <joses.jr@gmail.com>
2019-09-11 18:32:43 -0300

    Access key is now stored and can be reused

--- sdm-authenticate.sh
+++ sdm-authenticate.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+AUTH_DIR=~/.sdm-cli
+AUTH_FILE=$AUTH_DIR/auth
+
+if [ "$1" == "-r" ]; then
+    if [ ! -f $AUTH_FILE ]; then
+        echo "Access key not stored. Please authenticate." >&2
+        exit 1
+    fi
+
+    EXPIRATION_DATE=$(cat $AUTH_FILE | grep expiration_date | cut -d":" -f2)
+    ACCESS_KEY=$(cat $AUTH_FILE | grep access_key | cut -d":" -f2)
+
+    if (( $(date +%s) > $EXPIRATION_DATE )); then
+        echo "Access key expired. Please authenticate again." >&2
+        exit 1
+    fi
+    echo $ACCESS_KEY
+    exit
+fi
+
 if (( $# < 2 )); then
-    echo "Usage: sdm-authenticate.sh <USERNAME> <PASSWORD> [OPTIONS]"
+    echo "Usage:"
+    echo -e "\tsdm-authenticate.sh -r"
+    echo -e "\tsdm-authenticate.sh <USERNAME> <PASSWORD> [OPTIONS]"
     echo -e "Options:"
+    echo -e "    -r\tRevalidate stored access key"
     echo -e "    -x\tPrint full result"
     exit 1
 fi
@@ -20,11 +43,16 @@ RESPONSE=$(curl -s \
    http://sjkap754:8050/caisd-rest/rest_access)
 
 ACCESS_KEY=$(echo $RESPONSE | sed "s/.*access_key>\([0-9]\+\).*/\1/g")
+EXPIRATION_DATE=$(echo $RESPONSE | sed "s/.*expiration_date>\([0-9]\+\).*/\1/g")
 
 if [ -n "$(echo $ACCESS_KEY | sed s/[0-9]*//g)" ]; then
     echo "Authentication failure." >&2
     exit 1
 fi
+
+mkdir -p $AUTH_DIR
+echo "access_key: $ACCESS_KEY" > $AUTH_FILE
+echo "expiration_date: $EXPIRATION_DATE" >> $AUTH_FILE
 
 if [ "$ARGS" == "-x" ]; then
     echo $RESPONSE
